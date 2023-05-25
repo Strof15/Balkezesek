4236c276fd85a69fe72a43a5d0e80066c927f82d
Kristof <Kristof@DESKTOP-C9STJNN>
2023-05-25 11:48:38 +0200

    4. ora vege

--- 2023.05.25/Program.cs
+++ 2023.05.25/Program.cs
@@ -1,1 +1,58 @@
 ﻿
+
+
+
+
+
+
+
+using _2023._05._25;
+
+
+internal class prog {
+
+    static List<Jatekos> balkezeseklista = new List<Jatekos>();
+
+    static void Main(string[] args)
+    {
+        Beolvasas();
+        harmasfeladat();
+    }
+
+
+
+    static void Beolvasas()
+    {
+        StreamReader olvas = new StreamReader("balkezesek.csv");
+        balkezeseklista.Skip(1);
+        while (!olvas.EndOfStream)
+        {
+            string[] adatok = olvas.ReadLine().Split(';');
+            balkezeseklista.Add(adatok[0], adatok[1], adatok[2], int.Parse(adatok[3]), int.Parse(adatok[4]));
+
+        }
+
+
+
+        olvas.Close();
+
+
+
+    }
+    static void harmasfeladat() 
+    
+    {
+        Console.WriteLine(balkezeseklista.Count());
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ 2023.05.25/jatekos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2023._05._25
+{
+    internal class Jatekos
+    {
+        string nev;
+        string elso;
+        string utolso;
+        int suly;
+        int magassag;
+
+        public Jatekos(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elso = elso;
+            this.utolso = utolso;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev; }
+        public string Elso { get => elso; }
+        public string Utolso { get => utolso;  }
+        public int Suly { get => suly; }
+        public int Magassag { get => magassag; }
+
+        //nev,elso  utolso suly magassag
+
+
+
+
+
+    }
+}
